d2305c1e28b5dca25c8393cbcedc9c17106d7412
kryssyco <kjshepard70@hotmail.com>
2020-02-18 01:09:34 -0700

    Added markdown file for adding users and tweets end of day 1 DS9

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,12 +1,19 @@
 """ Code for our App """
 
 from flask import Flask
+from .models import DB
 
 # Make app factory
 
 def create_app():
     app = Flask(__name__)
 
+    # Add config for database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Have the database know about the app
+    DB.init_app(app)
+    
     @app.route("/")
     def root():
         return 'Welcome to Twitoff!!!'
