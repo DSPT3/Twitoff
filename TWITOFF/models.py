e8865b9d1e911454b85fc99afddbfd6b9b8423cb
kryssyco <kjshepard70@hotmail.com>
2020-02-21 06:53:48 -0700

    Made additional changed to the db schema in models.py

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -11,7 +11,7 @@ class User(DB.Model):
     '''
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
-    #newest_tweet_id = DB.Column(DB.BigInteger)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
