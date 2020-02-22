bd9abe484958ff615c2e936120a55124d172d30f
kryssyco <kjshepard70@hotmail.com>
2020-02-21 19:29:36 -0700

    Commit 2 datebase and flask app are working

---
+++ TWITOFF/app.py
@@ -0,0 +1,26 @@
+""" Code for our app """
+
+from flask import Flask, render_template
+from flask_migrate import Migrate
+from .models import DB
+# Make the app factory
+
+def create_app():
+    app = Flask(__name__)
+    migrate = Migrate()
+
+    # Add config for the database
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Let the database know about the app
+
+    DB.init_app(app)
+    migrate.init_app(app, DB)
+
+    #Routes
+
+    @app.route('/')
+    def root():
+        return "Welcome to Twitoff!!!"
+    return app
